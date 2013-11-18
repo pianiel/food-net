8714799099c52ac58b1aa5f4a32dbea0b97626c1
Piotrek <pianiel@example.com>
2013-11-19 00:14:12 +0100

    fixed sql edges query to include edge weight

--- gephiproj/importer.sql
+++ gephiproj/importer.sql
@@ -2,10 +2,13 @@
 select id, name as label from ingredients;
 
 -- edges query
-select distinct a.ingredient_id as source, b.ingredient_id as target 
+select distinct 
+a.ingredient_id as source, 
+b.ingredient_id as target, 
+count(a.ingredient_id) as weight
 from recipes_ingredients as a
 join recipes_ingredients as b 
 using (recipe_id)
 where a.ingredient_id < b.ingredient_id
-order by source, target
-group by source, target;
+group by source, target
+order by source, target;
